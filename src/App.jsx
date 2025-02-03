d1415d616103dcee5a5d4e6f9eee0a0e4dabdfb5
Manuel Leyva de la Morena <leyvamanu@gmail.com>
2025-02-03 22:47:07 +0100

    new logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import logo from './assets/logo.png'
+import logo from './assets/logo-bg-transparent.svg'
 
 export default function App() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+        <div className="flex min-h-screen items-center justify-center bg-[linear-gradient(64deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0.749)_67.5%)] text-white">
             <img src={logo} alt="Manuel Leyva Desarrollador Web - Logo"/>
         </div>
     );

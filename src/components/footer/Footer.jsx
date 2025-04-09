ff89cf601c7deb59c376b964e4ff6396eeb33180
Manu Leyva <leyvamanu@gmail.com>
2025-04-09 09:44:11 +0200

    FIX: footer sm

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <footer className="text-gray-300 py-8">
             <div className="max-w-7xl mx-auto px-4 border-t border-t-orange-app pt-4">
-                <div className="flex flex-col md:flex-row justify-between items-start">
+                <div className="flex flex-col justify-center md:flex-row md:justify-between items-center md:items-start gap-4 md:gap-8">
                     <div className="flex justify-center">
                         <a href="/" className="hidden md:block">
                             <img src={logo} alt="Manu Leyva - Desarrollador Web - Logo" className="w-80" />
